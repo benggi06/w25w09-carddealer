c5f7f94196b98b1c626f8019bad877d05fe127e1
benggi06 <rldnd0616@gmail.com>
2025-11-10 15:24:56 +0900

    🎉프로젝트 시작

---
+++ src/test/kotlin/kr/ac/kumoh/s20220518/w25w09_carddealer/W25w09CarddealerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220518.w25w09_carddealer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W25w09CarddealerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
